6f8ef7645fd9819042ad226f79734e57e3f4eaed
Bacon-is <seancollins11@hotmail.com>
2024-07-21 01:39:13 -0700

    Created a canvas allowing for much simpler placement of Assets Created Text for Odometer and Air Tem

--- GUI.py
+++ GUI.py
@@ -3,20 +3,42 @@ from PIL import ImageTk, Image
 
 root = Tk()
 
-#Open's Asset Images
-BackgroundImg = Image.open('Background.png')
-SpeedoNeedleImg = Image.open('Speedometer Needle.png')
+#set's Display Size
+root.geometry("1024x600")
+#Set's Title
+root.title('RX-7 Dash')
 
-angle = 10
+#Set's the text font and size
+GlobalFont = ('Calibri',20)
+
+#Rotates Gauge Needles on Screen
+SpeedoNeedleAngle = -90
+
 
 #Positions Gauge Needles on Screen
-SpeedoNeedlePos = (0,0)
+SpeedoNeedlePos = (512,300)
+MileagePos = (200,500)
+AirTempPos = (825,500)
 
-#Places Assets above the background
-BackgroundImg.paste(SpeedoNeedleImg,(SpeedoNeedlePos), SpeedoNeedleImg.rotate(angle))
-tkimage = ImageTk.PhotoImage(BackgroundImg)
-panel1 = Label(root, image=tkimage)
-panel1.grid(row=0, column=2, sticky=E)
 
+#Positions Text on Screen
+
+#Open's Asset Image Files
+BackgroundAsset = ImageTk.PhotoImage(file='Background.png')
+SpeedoNeedleAsset = ImageTk.PhotoImage(file='Speedometer Needle.png')
+
+#Create's Labels
+Mileage = ("231259")
+AirTemp = ("80")
+
+#Creates the Canvas
+canvas = Canvas(root, width=1024, height=600, bg='white')
+canvas.pack(anchor=CENTER,expand=True)
+
+#adds pictures and text to the canvas
+canvas.create_image(SpeedoNeedlePos, image=BackgroundAsset)
+canvas.create_image(SpeedoNeedlePos, image=SpeedoNeedleAsset)
+canvas.create_text(MileagePos, text=Mileage, fill='White',font=GlobalFont,)
+canvas.create_text(AirTempPos, text=AirTemp, fill='White',font=GlobalFont,)
 
 root.mainloop()
